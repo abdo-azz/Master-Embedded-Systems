0429ecc4946daedafb13bcff340955c14bba59f8
Abdel Rahman Abdel Moez Ahmed <81767025+abdo882@users.noreply.github.com>
2022-09-21 22:08:41 +0200

    ass strings
    ass strings

--- ass3 ex1 string.c
+++ ass3 ex1 string.c
@@ -1,15 +1,20 @@
 
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void)
 {
-	char c[100];
-	int i;
+	char c[100],al;
+	int i,j=0;
 	printf("enter a string: \n ");
 	fflush(stdin);fflush(stdout);
 	gets(c);
-	for (i=0;c[i]!='\0';++i);
-	printf("length of string =%d \n",i);
+	printf("enter a character to find frequency: ");
+    scanf("%c",&al);
+	for (i=0;c[i]!='\0';++i)
+	{
+	if(c[i]==al)
+        j++;
+	}
+	printf("frequency of %c =%d \n",al,j);
 }
