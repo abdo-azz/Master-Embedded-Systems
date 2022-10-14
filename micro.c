294c697f0842754d8e0ae770f520f654283cf2ee
Abdel Rahman Abdel Moez Ahmed <81767025+abdo882@users.noreply.github.com>
2022-10-14 18:44:35 +0200

    Add files via upload
    Ass.micro

---
+++ micro.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : micro.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define pi 3.1415
+#define area(r) r*r*pi
+int main(void)
+{
+    int r;
+    printf("Enter the radius: ");
+    scanf("%d",&r);
+printf("the area = %f",area(r));
+}
